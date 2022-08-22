a960b00667f96efeab1c0d15300e1f3ec804f4b8
iamtalib13 <talibsheikh16@gmail.com>
2022-08-22 23:08:23 +0530

    Added - Mobile Format

--- src/com/bridgelabz/userregistration/Regex.java
+++ src/com/bridgelabz/userregistration/Regex.java
@@ -11,24 +11,28 @@ public class Regex {
         String regex_firstname = "[A-Z]{1}(?=.*[a-z]).{2,}";
         String regex_lastname = "[A-Z]{1}(?=.*[a-z]).{2,}";
         String regex_email = "^[a-zA-Z]+([._+-]{0,1}[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[(com)|(co)|(net)]+(?:\\.[a-z]{2,}){0,1}$";
-
+        String regex_mobile_number="^\\d{2}(\\s{1}\\d{10})$";
 
         String firstname = "Talib";
         String lastname = "Sheikh";
         String email = "talibsheikh456@gmail.com";
+        String mobile = "91 8793679452";
 
         Pattern pattern = Pattern.compile(regex_firstname);
         Pattern pattern1 = Pattern.compile(regex_lastname);
         Pattern pattern2 = Pattern.compile(regex_email);
+        Pattern pattern3 = Pattern.compile(regex_mobile_number);
 
 
         Matcher matcher = pattern.matcher(firstname);
         Matcher matcher1 = pattern1.matcher(lastname);
         Matcher matcher2 = pattern2.matcher(email);
+        Matcher matcher3 = pattern3.matcher(mobile);
 
         System.out.println("Pattern match for firstname is: " + matcher.find());
         System.out.println("Pattern match lastname is: " + matcher1.find());
         System.out.println("Pattern match email is: " + matcher2.find());
+        System.out.println("Pattern match mobile is: " + matcher3.find());
 
 
     }

c0a2fbd04ae9af62cb54aeb3d99e05188cc2a27b
iamtalib13 <talibsheikh16@gmail.com>
2022-08-22 23:01:50 +0530

    Added - Enter Valid Last name

--- src/com/bridgelabz/userregistration/Regex.java
+++ src/com/bridgelabz/userregistration/Regex.java
@@ -5,22 +5,25 @@ import java.util.regex.Pattern;
 
 public class Regex {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
         System.out.println("Welcome to User Registration problem");
         String regex_firstname = "[A-Z]{1}(?=.*[a-z]).{2,}";
+        String regex_lastname = "[A-Z]{1}(?=.*[a-z]).{2,}";
 
         String firstname = "Talib";
+        String lastname = "Sheikh";
 
 
         Pattern pattern = Pattern.compile(regex_firstname);
-
+        Pattern pattern1 = Pattern.compile(regex_lastname);
 
 
         Matcher matcher = pattern.matcher(firstname);
+        Matcher matcher1 = pattern1.matcher(lastname);
 
-        System.out.println("Pattern match is: " + matcher.find());
+        System.out.println("Pattern match for firstname is: " + matcher.find());
+        System.out.println("Pattern match lastname is: " + matcher1.find());
 
 
     }

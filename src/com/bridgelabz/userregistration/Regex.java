d40b8bb1d88f29a2055837c7ce4435ba530b6db8
iamtalib13 <talibsheikh16@gmail.com>
2022-08-22 23:05:23 +0530

    Added - Enter Valid Email

--- src/com/bridgelabz/userregistration/Regex.java
+++ src/com/bridgelabz/userregistration/Regex.java
@@ -10,20 +10,25 @@ public class Regex {
         System.out.println("Welcome to User Registration problem");
         String regex_firstname = "[A-Z]{1}(?=.*[a-z]).{2,}";
         String regex_lastname = "[A-Z]{1}(?=.*[a-z]).{2,}";
+        String regex_email = "^[a-zA-Z]+([._+-]{0,1}[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[(com)|(co)|(net)]+(?:\\.[a-z]{2,}){0,1}$";
+
 
         String firstname = "Talib";
         String lastname = "Sheikh";
-
+        String email = "talibsheikh456@gmail.com";
 
         Pattern pattern = Pattern.compile(regex_firstname);
         Pattern pattern1 = Pattern.compile(regex_lastname);
+        Pattern pattern2 = Pattern.compile(regex_email);
 
 
         Matcher matcher = pattern.matcher(firstname);
         Matcher matcher1 = pattern1.matcher(lastname);
+        Matcher matcher2 = pattern2.matcher(email);
 
         System.out.println("Pattern match for firstname is: " + matcher.find());
         System.out.println("Pattern match lastname is: " + matcher1.find());
+        System.out.println("Pattern match email is: " + matcher2.find());
 
 
     }
